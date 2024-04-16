459451c88e19270128230c7d0d26151ee987dffc
ritikjee <ritikj.ug21.ee@nitp.ac.in>
2024-04-17 00:40:43 +0530

    test-project

---
+++ lib/constants.ts
@@ -0,0 +1,13 @@
+export type EditorBtns =
+  | "text"
+  | "container"
+  | "section"
+  | "contactForm"
+  | "paymentForm"
+  | "link"
+  | "2Col"
+  | "video"
+  | "__body"
+  | "image"
+  | null
+  | "3Col";
